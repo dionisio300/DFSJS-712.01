599728364a2331db8c41053698a0b0c0f2f1f703
dionisio300 <dionisioassis80@gmail.com>
2025-05-03 15:56:37 -0300

    Aula final

---
+++ Node e Git/ProjetoContaBancaria/index.mjs
@@ -0,0 +1,6 @@
+/*
+Crie um banco de dados para representar um cliente de um banco.
+1 - tela de login: Crie uma página de login pedindo usuario e senha.
+faça uma consulta no banco de dados para verificar se o usuário e a senha foram digitados corretamente.
+2 - Crie a tela de página inicial: Essa tela deve mostrar o saldo atual do cliente e 4 opções: Sacar, Depositar, transferir e deletar cartão.
+*/
